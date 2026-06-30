a991ad9b680d369b83a0800ec651620453d026b8
YAGO BACK SCOZ <yago_scoz@estudante.sesisenai.org.br>
2026-06-30 16:05:12 -0300

    Alteracao de pastas

---
+++ Banco/Scripts/QuerrysCriacao.sql
@@ -0,0 +1,24 @@
+-- Fluxo de caixa
+--     entradas realizadas
+-- entradas previstas
+-- saídas realizada
+-- saídas previstas
+-- saldo inicial
+-- saldo final
+-- Dividido em:
+-- 30 dias
+-- 60 dias
+-- 90 dias
+
+-- Gerador de notas fiscais
+--     Informação empresas
+-- Id da nota
+-- data de emissão
+
+-- Lista da compra/venda -> deve conter id do item, nome e quantidade, valor produto
+
+-- descrição da nota (se houver)
+
+-- valor total
+-- tipo de pagamento
+-- Parcelas (se houver)
