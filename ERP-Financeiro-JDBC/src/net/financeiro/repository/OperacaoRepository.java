81f5f4253e57cef11687201044492e5860494a65
KAUÊ KREUZ DE MIRANDA <kaue_miranda@estudante.sesisenai.org.br>
2026-07-03 18:16:40 -0300

    feat: atualização de categorias adicionado

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/OperacaoRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/OperacaoRepository.java
@@ -27,4 +27,22 @@ public class OperacaoRepository {
             return null;
         }
     }
+
+    public Operacao atualizar(Operacao op){
+        String sql = "UPDATE Operacao SET id_operacao = ?, data_operacao = ?, status_operacao = ?, id_fornecedor_cliente = ?, id_funcionario = ? WHERE id_operacao = ?";
+
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
+            pr.setLong(1, op.getId_operacao());
+            pr.setString(2, op.getData_operacao());
+            pr.setString(3, op.getStatus_operacao());
+            pr.setLong(4, op.getId_fornecedor_cliente());
+            pr.setLong(5, op.getId_funcionario());
+            pr.setLong(6, op.getId_operacao());
+            pr.executeUpdate();
+            return op;
+        } catch(SQLException e){
+            System.out.println("Erro ao atualizar dados: " + e.getMessage());
+            return null;
+        }
+    }
 }
