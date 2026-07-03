5e02332eb5e4508b189078bb433baf1f2006e832
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-07-02 21:40:31 -0300

    feat: Validação da chave estrangeira adicionada em ProdutosService

--- ERP-Financeiro-JDBC/src/net/financeiro/service/ProdutosService.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/service/ProdutosService.java
@@ -5,6 +5,7 @@ import net.financeiro.exceptions.NadaInseridoException;
 import net.financeiro.exceptions.NomeInvalidoException;
 import net.financeiro.model.Categoria_Item;
 import net.financeiro.model.Produto;
+import net.financeiro.repository.Categoria_ItemRepository;
 import net.financeiro.repository.ProdutosRepository;
 
 import java.util.HashMap;
@@ -12,6 +13,8 @@ import java.util.List;
 
 public class ProdutosService {
     private final ProdutosRepository repository = new ProdutosRepository();
+    private final Categoria_ItemRepository categoriaItemRepository = new Categoria_ItemRepository();
+
     public Produto inserir(Produto ins) throws NomeInvalidoException {
         try{
             if(ins.getNome().trim().isEmpty()){
@@ -23,8 +26,14 @@ public class ProdutosService {
             if(ins.getQuantidade_estoque() < 0){
                 throw new NomeInvalidoException("Erro: quantidade de produtos é negativa");
             }
+            if(categoriaItemRepository.buscarPorId(ins.getId_categoria_item()) == null){
+                throw new IdNaoEncontradoException("Erro: chave estrangeira não encontrada!");
+            }
             return repository.inserir(ins);
         } catch(NomeInvalidoException e){
+            System.out.println(e.getMessage());
+            return null;
+        } catch (IdNaoEncontradoException e){
             System.out.println(e.getMessage());
             return null;
         }
@@ -43,6 +52,9 @@ public class ProdutosService {
             }
             if(repository.buscarPorId(atl.getId_produto()) == null){
                 throw new IdNaoEncontradoException("Erro: id não encontrado!");
+            }
+            if(categoriaItemRepository.buscarPorId(atl.getId_categoria_item()) == null){
+                throw new IdNaoEncontradoException("Erro: chave estrangeira não encontrada!");
             }
             return repository.atualizar(atl);
         } catch(NomeInvalidoException e){
