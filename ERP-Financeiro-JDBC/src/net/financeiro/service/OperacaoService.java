27bf4c870edbf773510836c3f63b1cbec4ef3f4a
KAUÊ KREUZ DE MIRANDA <kaue_miranda@estudante.sesisenai.org.br>
2026-07-06 15:31:08 -0300

    feat: finalização do Service de Operação

--- ERP-Financeiro-JDBC/src/net/financeiro/service/OperacaoService.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/service/OperacaoService.java
@@ -3,8 +3,11 @@ package net.financeiro.service;
 import net.financeiro.exceptions.AtributoInvalidoException;
 import net.financeiro.exceptions.FkNaoEncontradaException;
 import net.financeiro.exceptions.IdNaoEncontradoException;
+import net.financeiro.exceptions.NadaInseridoException;
 import net.financeiro.model.Operacao;
 import net.financeiro.repository.OperacaoRepository;
+
+import java.util.List;
 
 public class OperacaoService {
     private final OperacaoRepository repository = new OperacaoRepository();
@@ -24,6 +27,55 @@ public class OperacaoService {
         }catch(AtributoInvalidoException | FkNaoEncontradaException e){
             System.out.println(e.getMessage());
             return null;
+        }
+    }
+
+    public Operacao atualizar(Operacao op){
+        try{
+            if(repository.buscarPorId(op.getId_operacao()) == null){
+                throw new IdNaoEncontradoException("Erro: id não encontrado!");
+            }
+            if(!op.getStatus_operacao().equals("Pendente") || !op.getStatus_operacao().equals("Concluída")){
+                throw new AtributoInvalidoException("Erro: atributo inválido!");
+            }
+            if(repository.buscarPorId(op.getId_fornecedor_cliente()) == null){
+                throw new FkNaoEncontradaException("Erro: chave estrangeira não encontrada!");
+            }
+            if(repository.buscarPorId(op.getId_funcionario()) == null){
+                throw new FkNaoEncontradaException("Erro: chave estrangeira não encontrada!");
+            }
+            return op;
+        }catch(IdNaoEncontradoException | AtributoInvalidoException | FkNaoEncontradaException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public List<Operacao> listarInfo(){
+        try{
+            List<Operacao> lista = repository.listarInfo();
+            if(lista.isEmpty()){
+                throw new NadaInseridoException("Erro: nada inserido no banco");
+            }
+            return lista;
+        }catch(NadaInseridoException e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean deletar(Long id){
+        try{
+            if(repository.buscarPorId(id) == null){
+                throw new IdNaoEncontradoException("Erro: id não encontrado!");
+            }
+            if(repository.listarInfo().isEmpty()){
+                throw new NadaInseridoException("Erro: nada inserido no banco");
+            }
+            return true;
+        }catch(IdNaoEncontradoException | NadaInseridoException e){
+            System.out.println(e.getMessage());
+            return false;
         }
     }
 }
