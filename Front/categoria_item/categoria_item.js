b9ce7bc7418953f46de7369733f01a219cc2985e
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-07-14 21:37:41 -0300

    feat: front de categoria item melhorado

--- Front/categoria_item/categoria_item.js
+++ Front/categoria_item/categoria_item.js
@@ -1,5 +1,6 @@
 document.getElementById("formPost").addEventListener("submit", async (event) => {
     event.preventDefault();
+    const resultado = document.getElementById("resultados");
     try{
         const obj = {"nome" : document.getElementById("nomePost").value};
         const resposta = await fetch("http://localhost:8085/categoria_item", {
@@ -8,15 +9,16 @@ document.getElementById("formPost").addEventListener("submit", async (event) => 
             body : JSON.stringify(obj)
         });
         const retorno = await resposta.json();
+        resultado.innerHTML = `Inserido com sucesso!`;
     } catch (error) {
-        console.log(error);
+        resultado.innerHTML = `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">Erro ao inserir!</p>`;
     }
 });
 
 
 document.getElementById("getTodos").addEventListener("click", async (event) => {
     event.preventDefault();
-    const resultado = document.getElementById("resultadoBusca");
+    const resultado = document.getElementById("resultados");
     try{
         const resposta = await fetch("http://localhost:8085/categoria_item", {
             method : "GET"
@@ -24,37 +26,37 @@ document.getElementById("getTodos").addEventListener("click", async (event) => {
         const retorno = await resposta.json();
         let res = "";
         for(let i = 0; i < retorno.length; i++){
-            res += `<p>> Id: ${retorno[i].id_categoria_item} | Nome: ${retorno[i].nome}<p><br>`;
+            res += `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">> Id: ${retorno[i].id_categoria_item} | Nome: ${retorno[i].nome}<p>`;
         }
         resultado.innerHTML = res;
     } catch (error) {
-        console.log(error);
+        resultado.innerHTML = `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">Erro ao listar!</p>`;
     }
 });
 
 document.getElementById("maiorVenda").addEventListener("click", async (event) => {
     event.preventDefault();
-    const resultado = document.getElementById("resultadoBusca");
+    const resultado = document.getElementById("resultados");
     try{
         const resposta = await fetch("http://localhost:8085/categoria_item/maiorVenda", {
             method : "GET"
         });
         const retorno = await resposta.json();
         let res = "";
-        let nomes = retorno["NomeCategoria"];
-        let vendas = retorno["VendaCategoria"];
+        const nomes = retorno["NomeCategoria"];
+        const vendas = retorno["VendaCategoria"];
         for(let i = 0; i < nomes.length; i++){
-            res += `<p>> Nome da categoria: ${nomes[i]} | Valor de venda: ${vendas[i]}<p><br>`;
+            res += `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">> Nome da categoria: ${nomes[i]} | Valor de venda: ${vendas[i]}<p>`;
         }
         resultado.innerHTML = res;
     } catch (error) {
-        console.log(error);
+        resultado.innerHTML = `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">Erro ao ordenar</p>!`;
     }
 });
 
 document.getElementById("menorVenda").addEventListener("click", async (event) => {
     event.preventDefault();
-    const resultado = document.getElementById("resultadoBusca");
+    const resultado = document.getElementById("resultados");
     try{
         const resposta = await fetch("http://localhost:8085/categoria_item/menorVenda", {
             method : "GET"
@@ -64,17 +66,17 @@ document.getElementById("menorVenda").addEventListener("click", async (event) =>
         let nomes = retorno["NomeCategoria"];
         let vendas = retorno["VendaCategoria"];
         for(let i = 0; i < nomes.length; i++){
-            res += `<p>> Nome da categoria: ${nomes[i]} | Valor de venda: ${vendas[i]}<p><br>`;
+            res += `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">> Nome da categoria: ${nomes[i]} | Valor de venda: ${vendas[i]}<p>`;
         }
         resultado.innerHTML = res;
     } catch (error) {
-        console.log(error);
+        resultado.innerHTML = `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">Erro ao ordenar!</p>`;
     }
 });
 
 document.getElementById("mediaVenda").addEventListener("click", async (event) => {
     event.preventDefault();
-    const resultado = document.getElementById("resultadoBusca");
+    const resultado = document.getElementById("resultados");
     try{
         const resposta = await fetch("http://localhost:8085/categoria_item/mediaVenda", {
             method : "GET"
@@ -84,16 +86,17 @@ document.getElementById("mediaVenda").addEventListener("click", async (event) =>
         let nomes = retorno["NomeCategoria"];
         let vendas = retorno["VendaCategoria"];
         for(let i = 0; i < nomes.length; i++){
-            res += `<p>> Nome da categoria: ${nomes[i]} | Valor médio de venda: ${vendas[i]}<p><br>`;
+            res += `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">> Nome da categoria: ${nomes[i]} | Valor médio de venda: ${vendas[i]}<p>`;
         }
         resultado.innerHTML = res;
     } catch (error) {
-        console.log(error);
+        resultado.innerHTML = `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">Erro ao ordenar!</p>`;
     }
 });
 
 document.getElementById("formPut").addEventListener("submit", async (event) => {
     event.preventDefault();
+    const resultado = document.getElementById("resultados");
     try{
         const id = document.getElementById("idPut").value;
         const obj = {"nome" : document.getElementById("nomePut").value};
@@ -103,19 +106,22 @@ document.getElementById("formPut").addEventListener("submit", async (event) => {
             body : JSON.stringify(obj)
         });
         const retorno = await resposta.json();
+        resultado.innerHTML = `Atualizado com sucesso!`;
     } catch (error) {
-        console.log(error);
+        resultado.innerHTML = `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">Erro ao atualizar!</p>`;
     }
 });
 
 document.getElementById("formDelete").addEventListener("submit", async (event) => {
     event.preventDefault();
+    const resultado = document.getElementById("resultados");
     try{
         const id = document.getElementById("idDelete").value;
         const resposta = await fetch(`http://localhost:8085/categoria_item/${id}`, {
             method : "DELETE"
         });
+        resultado.innerHTML = `Deletado com sucesso!`;
     } catch (error) {
-        console.log(error);
+        resultado.innerHTML = `<p style="background-color:#C5D8D1; color: black; padding: 1%; border: 3px solid white; margin: 1%">Erro ao deletar!</p>`;
     }
 });
