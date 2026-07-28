a0d04467fbbaaa3df6c0dd300e6ea730f513c72c
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-07-28 18:51:43 -0300

    feat: criando um usuário para cada membro do grupo

--- Banco/Scripts/usuarios.sql
+++ Banco/Scripts/usuarios.sql
@@ -12,4 +12,24 @@ create user 'erp-financeiro'@'%' identified by 'valentim123';
 
 grant select, insert, update, delete, create on ERP_Financeiro.* to 'erp-financeiro'@'%' ;
 
+create user 'leticia'@'%' identified by 'valentim123';
+
+grant all privileges on ERP_Financeiro.* to 'leticia'@'%' ;
+
+create user 'matheus'@'%' identified by 'valentim123';
+
+grant select, insert, update, create on ERP_Financeiro.* to 'matheus'@'%' ;
+
+create user 'ryan'@'%' identified by 'valentim123';
+
+grant select on ERP_Financeiro.* to 'ryan'@'%' ;
+
+create user 'kaue'@'%' identified by 'valentim123';
+
+grant select on ERP_Financeiro.* to 'kaue'@'%' ;
+
+create user 'yago'@'%' identified by 'valentim123';
+
+grant select on ERP_Financeiro.* to 'yago'@'%' ;
+
 flush privileges;
