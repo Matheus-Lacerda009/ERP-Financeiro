fd1f845a97d7371ba5230814669701bb5095d272
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-07-29 19:45:36 -0300

    fix: removendo try-catches do repository e service de Produto

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/ProdutosRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/ProdutosRepository.java
@@ -12,82 +12,62 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ProdutosRepository {
-    public Produto inserir(Produto ins){
+    public Produto inserir(Produto ins) throws SQLException {
         String sql = "INSERT INTO Produto (nome, valor, descricao, quantidade_estoque, id_categoria_item) VALUES (?, ?, ?, ?, ?)";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-            pr.setString(1, ins.getNome());
-            pr.setDouble(2, ins.getValor());
-            pr.setString(3, ins.getDescricao());
-            pr.setInt(4, ins.getQuantidade_estoque());
-            pr.setLong(5, ins.getId_categoria_item());
-            pr.executeUpdate();
-            ResultSet rs = pr.getGeneratedKeys();
-            rs.next();
-            ins.setId_produto(rs.getLong("GENERATED_KEY"));
-            return ins;
-        } catch(SQLException e){
-            System.out.println("Erro ao inserir: " + e.getMessage());
-            return null;
-        }
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        pr.setString(1, ins.getNome());
+        pr.setDouble(2, ins.getValor());
+        pr.setString(3, ins.getDescricao());
+        pr.setInt(4, ins.getQuantidade_estoque());
+        pr.setLong(5, ins.getId_categoria_item());
+        pr.executeUpdate();
+        ResultSet rs = pr.getGeneratedKeys();
+        rs.next();
+        ins.setId_produto(rs.getLong("GENERATED_KEY"));
+        return ins;
     }
 
-    public Produto atualizar(Produto atl){
+    public Produto atualizar(Produto atl) throws SQLException {
         String sql = "UPDATE Produto SET nome = ?, valor = ?, descricao = ?, quantidade_estoque = ?, id_categoria_item = ? WHERE id_produto = ?";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            pr.setString(1, atl.getNome());
-            pr.setDouble(2, atl.getValor());
-            pr.setString(3, atl.getDescricao());
-            pr.setInt(4, atl.getQuantidade_estoque());
-            pr.setLong(5, atl.getId_categoria_item());
-            pr.setLong(6, atl.getId_produto());
-            pr.executeUpdate();
-            return atl;
-        } catch(SQLException e){
-            System.out.println("Erro ao atualizar: " + e.getMessage());
-            return null;
-        }
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        pr.setString(1, atl.getNome());
+        pr.setDouble(2, atl.getValor());
+        pr.setString(3, atl.getDescricao());
+        pr.setInt(4, atl.getQuantidade_estoque());
+        pr.setLong(5, atl.getId_categoria_item());
+        pr.setLong(6, atl.getId_produto());
+        pr.executeUpdate();
+        return atl;
     }
 
-    public boolean deletar(Long id){
+    public boolean deletar(Long id) throws SQLException {
         String sql = "update Produto set ativo = false WHERE id_produto = ?";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            pr.setLong(1, id);
-            pr.executeUpdate();
-            return true;
-        } catch(SQLException e){
-            System.out.println("Erro ao deletar: " + e.getMessage());
-            return false;
-        }
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        pr.setLong(1, id);
+        pr.executeUpdate();
+        return true;
     }
 
-    public boolean reativar(Long id){
+    public boolean reativar(Long id) throws SQLException {
         String sql = "update Produto set ativo = true WHERE id_produto = ?";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            pr.setLong(1, id);
-            pr.executeUpdate();
-            return true;
-        } catch(SQLException e){
-            System.out.println("Erro ao reativar: " + e.getMessage());
-            return false;
-        }
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        pr.setLong(1, id);
+        pr.executeUpdate();
+        return true;
     }
 
-    public List<Produto> listarInfo(){
+    public List<Produto> listarInfo() throws SQLException {
         String sql = "SELECT Produto.*, Categoria_Item.nome FROM Produto join Categoria_Item on Categoria_Item.id_categoria_item = Produto.id_categoria_item where Produto.ativo = true";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            List<Produto> lista = new ArrayList<>();
-            ResultSet rs = pr.executeQuery();
-            while(rs.next()){
-                lista.add(new Produto(rs.getLong("id_produto"), rs.getLong("id_categoria_item"), rs.getString("Produto.nome"), rs.getString("descricao"), rs.getDouble("valor"), rs.getInt("quantidade_estoque"), rs.getString("Categoria_Item.nome")));
-            }
-            return lista;
-        } catch(SQLException e){
-            System.out.println("Erro ao listar: " + e.getMessage());
-            return null;
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        List<Produto> lista = new ArrayList<>();
+        ResultSet rs = pr.executeQuery();
+        while(rs.next()){
+            lista.add(new Produto(rs.getLong("id_produto"), rs.getLong("id_categoria_item"), rs.getString("Produto.nome"), rs.getString("descricao"), rs.getDouble("valor"), rs.getInt("quantidade_estoque"), rs.getString("Categoria_Item.nome")));
         }
+        return lista;
     }
 
-    public HashMap<String, List<String>> maiorVenda(){
+    public HashMap<String, List<String>> maiorVenda() throws SQLException {
         String sql = "SELECT sum(\n" +
                 "        p.valor * i.quantidade_produtos\n" +
                 "    ) as 'Venda por produto', p.nome as 'Nome produto'\n" +
@@ -100,25 +80,21 @@ public class ProdutosRepository {
                 "ORDER BY sum(\n" +
                 "        p.valor * i.quantidade_produtos\n" +
                 "    ) desc";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            ResultSet rs = pr.executeQuery();
-            HashMap<String, List<String>> lista = new HashMap<>();
-            List<String> nomeCategoria = new ArrayList<>();
-            List<String> vendaCategoria = new ArrayList<>();
-            lista.put("NomeProduto", nomeCategoria);
-            lista.put("VendaProduto", vendaCategoria);
-            while(rs.next()){
-                lista.get("NomeProduto").add(rs.getString("Nome produto"));
-                lista.get("VendaProduto").add(rs.getString("Venda por produto"));
-            }
-            return lista;
-        } catch(SQLException e){
-            System.out.println("Erro ao listar: " + e.getMessage());
-            return null;
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        ResultSet rs = pr.executeQuery();
+        HashMap<String, List<String>> lista = new HashMap<>();
+        List<String> nomeCategoria = new ArrayList<>();
+        List<String> vendaCategoria = new ArrayList<>();
+        lista.put("NomeProduto", nomeCategoria);
+        lista.put("VendaProduto", vendaCategoria);
+        while(rs.next()){
+            lista.get("NomeProduto").add(rs.getString("Nome produto"));
+            lista.get("VendaProduto").add(rs.getString("Venda por produto"));
         }
+        return lista;
     }
 
-    public HashMap<String, List<String>> menorVenda(){
+    public HashMap<String, List<String>> menorVenda() throws SQLException {
         String sql = "SELECT sum(\n" +
                 "        p.valor * i.quantidade_produtos\n" +
                 "    ) as 'Venda por produto', p.nome as 'Nome produto'\n" +
@@ -131,25 +107,21 @@ public class ProdutosRepository {
                 "ORDER BY sum(\n" +
                 "        p.valor * i.quantidade_produtos\n" +
                 "    ) asc";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            ResultSet rs = pr.executeQuery();
-            HashMap<String, List<String>> lista = new HashMap<>();
-            List<String> nomeCategoria = new ArrayList<>();
-            List<String> vendaCategoria = new ArrayList<>();
-            lista.put("NomeProduto", nomeCategoria);
-            lista.put("VendaProduto", vendaCategoria);
-            while(rs.next()){
-                lista.get("NomeProduto").add(rs.getString("Nome produto"));
-                lista.get("VendaProduto").add(rs.getString("Venda por produto"));
-            }
-            return lista;
-        } catch(SQLException e){
-            System.out.println("Erro ao listar: " + e.getMessage());
-            return null;
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        ResultSet rs = pr.executeQuery();
+        HashMap<String, List<String>> lista = new HashMap<>();
+        List<String> nomeCategoria = new ArrayList<>();
+        List<String> vendaCategoria = new ArrayList<>();
+        lista.put("NomeProduto", nomeCategoria);
+        lista.put("VendaProduto", vendaCategoria);
+        while(rs.next()){
+            lista.get("NomeProduto").add(rs.getString("Nome produto"));
+            lista.get("VendaProduto").add(rs.getString("Venda por produto"));
         }
+        return lista;
     }
 
-    public HashMap<String, List<String>> mediaVendas(){
+    public HashMap<String, List<String>> mediaVendas() throws SQLException {
         String sql = "SELECT avg(\n" +
                 "        p.valor * i.quantidade_produtos\n" +
                 "    ) as 'Média Venda por produto', p.nome as 'Nome produto'\n" +
@@ -162,36 +134,28 @@ public class ProdutosRepository {
                 "ORDER BY sum(\n" +
                 "        p.valor * i.quantidade_produtos\n" +
                 "    ) asc";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            ResultSet rs = pr.executeQuery();
-            HashMap<String, List<String>> lista = new HashMap<>();
-            List<String> nomeCategoria = new ArrayList<>();
-            List<String> vendaCategoria = new ArrayList<>();
-            lista.put("NomeProduto", nomeCategoria);
-            lista.put("VendaProduto", vendaCategoria);
-            while(rs.next()){
-                lista.get("NomeProduto").add(rs.getString("Nome produto"));
-                lista.get("VendaProduto").add(rs.getString("Média Venda por produto"));
-            }
-            return lista;
-        } catch(SQLException e){
-            System.out.println("Erro ao listar: " + e.getMessage());
-            return null;
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        ResultSet rs = pr.executeQuery();
+        HashMap<String, List<String>> lista = new HashMap<>();
+        List<String> nomeCategoria = new ArrayList<>();
+        List<String> vendaCategoria = new ArrayList<>();
+        lista.put("NomeProduto", nomeCategoria);
+        lista.put("VendaProduto", vendaCategoria);
+        while(rs.next()){
+            lista.get("NomeProduto").add(rs.getString("Nome produto"));
+            lista.get("VendaProduto").add(rs.getString("Média Venda por produto"));
         }
+        return lista;
     }
 
-    public Produto buscarPorId(Long id){
+    public Produto buscarPorId(Long id) throws SQLException {
         String sql = "select * from Produto where id = ? where Produto.ativo = true";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            pr.setLong(1, id);
-            ResultSet rs = pr.executeQuery();
-            if(rs.next()){
-                return new Produto(rs.getLong("id_produto"), rs.getLong("id_categoria_item"), rs.getString("nome"), rs.getString("descricao"), rs.getDouble("valor"), rs.getInt("quantidade_estoque"));
-            } else {
-                return null;
-            }
-        } catch(SQLException e){
-            System.out.println("Erro na busca por ID: " + e.getMessage());
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        pr.setLong(1, id);
+        ResultSet rs = pr.executeQuery();
+        if(rs.next()){
+            return new Produto(rs.getLong("id_produto"), rs.getLong("id_categoria_item"), rs.getString("nome"), rs.getString("descricao"), rs.getDouble("valor"), rs.getInt("quantidade_estoque"));
+        } else {
             return null;
         }
     }
