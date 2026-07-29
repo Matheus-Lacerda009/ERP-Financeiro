8111c5c5da0eaf48347867b220f5dc419ad31dd5
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-07-29 19:12:42 -0300

    fix: removendo try-catches do repository e service da ViewNotaFiscal

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/ViewNotaFiscalRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/ViewNotaFiscalRepository.java
@@ -10,18 +10,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ViewNotaFiscalRepository {
-    public List<ViewNotaFiscal> listarInfo(){
+    public List<ViewNotaFiscal> listarInfo() throws SQLException {
         String sql = "SELECT * FROM ViewNotaFiscal";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            ResultSet rs = pr.executeQuery();
-            List<ViewNotaFiscal> lista = new ArrayList<>();
-            while(rs.next()){
-                lista.add(new ViewNotaFiscal(rs.getLong("numero_nota_fiscal"), rs.getLong("id_fluxo_caixa"), rs.getLong("id_parceiro"), rs.getString("data_emissao"), rs.getString("nome_empresa_parceira"), rs.getString("documento_parceiro"), rs.getString("email_parceiro"), rs.getString("forma_pagamento"), rs.getString("banco_recebimento"), rs.getInt("parcelas")));
-            }
-            return lista;
-        } catch (SQLException e){
-            System.out.println("Erro ao listar: " + e.getMessage());
-            return null;
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        ResultSet rs = pr.executeQuery();
+        List<ViewNotaFiscal> lista = new ArrayList<>();
+        while(rs.next()){
+            lista.add(new ViewNotaFiscal(rs.getLong("numero_nota_fiscal"), rs.getLong("id_fluxo_caixa"), rs.getLong("id_parceiro"), rs.getString("data_emissao"), rs.getString("nome_empresa_parceira"), rs.getString("documento_parceiro"), rs.getString("email_parceiro"), rs.getString("forma_pagamento"), rs.getString("banco_recebimento"), rs.getInt("parcelas")));
         }
+        pr.close();
+        return lista;
     }
 }
--- ERP-Financeiro-JDBC/src/net/financeiro/service/ViewNotaFiscalService.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/service/ViewNotaFiscalService.java
@@ -4,21 +4,17 @@ import net.financeiro.exceptions.NadaInseridoException;
 import net.financeiro.model.ViewNotaFiscal;
 import net.financeiro.repository.ViewNotaFiscalRepository;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class ViewNotaFiscalService {
     private final ViewNotaFiscalRepository repository = new ViewNotaFiscalRepository();
 
-    public List<ViewNotaFiscal> listarInfo(){
-        try{
-            List<ViewNotaFiscal> lista = repository.listarInfo();
-            if(lista.isEmpty()){
-                throw new NadaInseridoException("Erro: nada inserido no banco");
-            }
-            return lista;
-        }catch(NadaInseridoException e){
-            System.out.println(e.getMessage());
-            return null;
+    public List<ViewNotaFiscal> listarInfo() throws NadaInseridoException, SQLException {
+        List<ViewNotaFiscal> lista = repository.listarInfo();
+        if(lista.isEmpty()){
+            throw new NadaInseridoException("Erro: nada inserido no banco");
         }
+        return lista;
     }
 }
