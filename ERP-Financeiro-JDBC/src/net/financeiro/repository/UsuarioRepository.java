bfd6f49d8f68534227a871d823c08e27a2416b94
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-07-30 19:20:20 -0300

    refactor: transfere método de login para UsuarioRepository

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/UsuarioRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/UsuarioRepository.java
@@ -34,4 +34,17 @@ public class UsuarioRepository {
         }
         return false;
     }
+
+    public boolean validacao(String nome, String senha){
+        String sql = "select * from Usuarios where nome = ? && senha = sha2(?, 256)";
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
+            pr.setString(1, nome);
+            pr.setString(2, senha);
+            ResultSet rs = pr.executeQuery();
+            return rs.next();
+        } catch(SQLException e){
+            System.out.println("Erro ao logar: " + e.getMessage());
+        }
+        return false;
+    }
 }
