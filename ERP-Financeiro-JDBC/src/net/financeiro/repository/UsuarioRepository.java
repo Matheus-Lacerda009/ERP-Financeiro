b14fcb054bb7a56874089bd3bd5d8fbb9a13cf3f
MATHEUS LACERDA <matheus_lacerda1@estudante.sesisenai.org.br>
2026-07-30 18:21:53 -0300

    fix: remove try-catches de UsuarioService e UsuarioRepository

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/UsuarioRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/UsuarioRepository.java
@@ -9,34 +9,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UsuarioRepository {
-    public boolean cadastrando(Usuario ins){
+    public boolean cadastrando(Usuario ins) throws SQLException {
         String sql = "insert into Usuarios(nome, senha, permissao) values (?, sha2(?, 256), ?)";
-        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)){
-            pr.setString(1, ins.getNome());
-            pr.setString(2, ins.getSenha());
-            pr.setInt(3, ins.getPermissao());
-            pr.executeUpdate();
+        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        pr.setString(1, ins.getNome());
+        pr.setString(2, ins.getSenha());
+        pr.setInt(3, ins.getPermissao());
+        if(pr.executeUpdate() != 0){
             return true;
-        } catch(SQLException e){
-            System.out.println("Erro ao inserir: " + e.getMessage());
-        } catch(PermissaoNegadaException e){
-            System.out.println(e.getMessage());
         }
         return false;
     }
 
-    public boolean isAdm(Usuario adm){
+    public boolean isAdm(Usuario adm) throws SQLException {
         String sqlVal = "select id from Usuarios where nome = ? && senha = sha2(?, 256)";
-        try(PreparedStatement prVal = Conexao.connecting().prepareStatement(sqlVal)){
-            prVal.setString(1, adm.getNome());
-            prVal.setString(2, adm.getSenha());
-            ResultSet rs = prVal.executeQuery();
-            rs.next();
-            if(rs.getLong("id") == 1L){
-                return true;
-            }
-        } catch(SQLException e){
-            System.out.println("Erro ao inserir: " + e.getMessage());
+        PreparedStatement prVal = Conexao.connecting().prepareStatement(sqlVal);
+        prVal.setString(1, adm.getNome());
+        prVal.setString(2, adm.getSenha());
+        ResultSet rs = prVal.executeQuery();
+        rs.next();
+        prVal.close();
+        if (rs.getLong("id") == 1L) {
+            return true;
         }
         return false;
     }
--- ERP-Financeiro-JDBC/src/net/financeiro/service/UsuarioService.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/service/UsuarioService.java
@@ -6,26 +6,24 @@ import net.financeiro.exceptions.ValorInvalidoException;
 import net.financeiro.model.Usuario;
 import net.financeiro.repository.UsuarioRepository;
 
+import java.sql.SQLException;
+
 public class UsuarioService {
     private final UsuarioRepository repository = new UsuarioRepository();
-    public boolean cadastrando(Usuario adm, Usuario ins){
-        try{
-            if(adm.getNome().trim().isEmpty() || ins.getNome().trim().isEmpty()){
-                throw new NadaInseridoException("Erro: nome vazio!");
-            }
-            if(adm.getSenha().trim().isEmpty() || ins.getSenha().trim().isEmpty()){
-                throw new NadaInseridoException("Erro: senha vazia!");
-            }
-            if(ins.getPermissao() < 1 || ins.getPermissao() > 3){
-                throw new ValorInvalidoException("Erro: permissão inválida!");
-            }
-            if(!repository.isAdm(adm)){
-                throw new PermissaoNegadaException("Erro: permissão negada!");
-            }
-            return repository.cadastrando(ins);
-        } catch(Exception e){
-            System.out.println(e.getMessage());
+
+    public boolean cadastrando(Usuario adm, Usuario ins) throws NadaInseridoException, ValorInvalidoException, SQLException {
+        if(adm.getNome().trim().isEmpty() || ins.getNome().trim().isEmpty()){
+            throw new NadaInseridoException("Erro: nome vazio!");
         }
-        return false;
+        if(adm.getSenha().trim().isEmpty() || ins.getSenha().trim().isEmpty()){
+            throw new NadaInseridoException("Erro: senha vazia!");
+        }
+        if(ins.getPermissao() < 1 || ins.getPermissao() > 3){
+            throw new ValorInvalidoException("Erro: permissão inválida!");
+        }
+        if(!repository.isAdm(adm)){
+            throw new PermissaoNegadaException("Erro: permissão negada!");
+        }
+        return repository.cadastrando(ins);
     }
 }
