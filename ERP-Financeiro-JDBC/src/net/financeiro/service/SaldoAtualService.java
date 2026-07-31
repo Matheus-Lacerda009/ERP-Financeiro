df732b490fe85dd5776184fca82a6ee36d384cb4
ryan_bylaardt <vonden.ryan@gmail.com>
2026-07-31 20:34:34 -0300

    fix: corrige throws em SaldoAtualService

--- ERP-Financeiro-JDBC/src/net/financeiro/service/SaldoAtualService.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/service/SaldoAtualService.java
@@ -4,10 +4,12 @@ import net.financeiro.exceptions.AvisoException;
 import net.financeiro.model.SaldoAtual;
 import net.financeiro.repository.SaldoAtualRepository;
 
+import java.sql.SQLException;
+
 public class SaldoAtualService {
     private final SaldoAtualRepository repository = new SaldoAtualRepository();
 
-    public SaldoAtual visualizar() {
+    public SaldoAtual visualizar() throws SQLException {
         SaldoAtual result = repository.visualizar();
         if(result.getValor() < 0) {
             System.out.println("Aviso: Saldo negativo!");
