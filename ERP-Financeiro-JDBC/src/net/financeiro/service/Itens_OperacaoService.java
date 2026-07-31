80b0c286cdd5e38ec6a1a7f9317a0f15ec40c1a8
vondenryan <vonden.ryan@gmail.com>
2026-07-31 13:06:26 -0300

    fix: remove try-catch de Itens_OperacaoService

--- ERP-Financeiro-JDBC/src/net/financeiro/service/Itens_OperacaoService.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/service/Itens_OperacaoService.java
@@ -9,6 +9,7 @@ import net.financeiro.repository.Itens_OperacaoRepository;
 import net.financeiro.repository.OperacaoRepository;
 import net.financeiro.repository.ProdutosRepository;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class Itens_OperacaoService {
@@ -16,96 +17,66 @@ public class Itens_OperacaoService {
     private final ProdutosRepository produtosRepository = new ProdutosRepository();
     private final OperacaoRepository operacaoRepository = new OperacaoRepository();
 
-    public Itens_Operacao inserir(Itens_Operacao ins) {
-        try {
-            if(ins.getQuantidade_produtos() < 0) {
-                throw new NadaInseridoException("Erro: quantidade de produtos negativa!");
-            }
-            if(produtosRepository.buscarPorId(ins.getId_produto()) == null) {
-                throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
-            }
-            if(operacaoRepository.buscarPorId(ins.getId_operacao()) == null) {
-                throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
-            }
-            return repository.inserir(ins);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
+    public Itens_Operacao inserir(Itens_Operacao ins) throws NadaInseridoException, FkNaoEncontradaException, SQLException {
+        if(ins.getQuantidade_produtos() < 0) {
+            throw new NadaInseridoException("Erro: quantidade de produtos negativa!");
         }
+        if(produtosRepository.buscarPorId(ins.getId_produto()) == null) {
+            throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
+        }
+        if(operacaoRepository.buscarPorId(ins.getId_operacao()) == null) {
+            throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
+        }
+        return repository.inserir(ins);
     }
 
-    public Itens_Operacao atualizar(Itens_Operacao atl, Long id) {
-        try {
-            if(atl.getQuantidade_produtos() < 0) {
-                throw new NadaInseridoException("Erro: quantidade de produtos negativa!");
-            }
-            if(produtosRepository.buscarPorId(atl.getId_produto()) == null) {
-                throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
-            }
-            if(operacaoRepository.buscarPorId(atl.getId_operacao()) == null) {
-                throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
-            }
-            if(repository.buscarPorId(id) == null) {
-                throw new IdNaoEncontradoException("Erro: ID não encontrado!");
-            }
-
-            return repository.atualizar(atl, id);
-        } catch(Exception e) {
-            System.out.println(e.getMessage());
-            return null;
+    public Itens_Operacao atualizar(Itens_Operacao atl, Long id) throws NadaInseridoException, FkNaoEncontradaException, IdNaoEncontradoException, SQLException {
+        if(atl.getQuantidade_produtos() < 0) {
+            throw new NadaInseridoException("Erro: quantidade de produtos negativa!");
         }
+        if(produtosRepository.buscarPorId(atl.getId_produto()) == null) {
+            throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
+        }
+        if(operacaoRepository.buscarPorId(atl.getId_operacao()) == null) {
+            throw new FkNaoEncontradaException("Erro: chave estrangeira inválida!");
+        }
+        if(repository.buscarPorId(id) == null) {
+            throw new IdNaoEncontradoException("Erro: ID não encontrado!");
+        }
+
+        return repository.atualizar(atl, id);
     }
 
-    public boolean deletar(Long id) {
-        try {
-            if(repository.buscarPorId(id) == null) {
-                throw new IdNaoEncontradoException("Erro: ID não encontrado!");
-            }
-            return repository.deletar(id);
-        } catch(IdNaoEncontradoException e) {
-            System.out.println(e.getMessage());
-            return false;
+    public boolean deletar(Long id) throws IdNaoEncontradoException, SQLException {
+        if(repository.buscarPorId(id) == null) {
+            throw new IdNaoEncontradoException("Erro: ID não encontrado!");
         }
+        return repository.deletar(id);
     }
 
-    public boolean reativar(Long id) {
-        try {
-            if(repository.buscarPorId(id) == null) {
-                throw new IdNaoEncontradoException("Erro: ID não encontrado!");
-            }
-            return repository.reativar(id);
-        } catch(IdNaoEncontradoException e) {
-            System.out.println(e.getMessage());
-            return false;
+    public boolean reativar(Long id) throws IdNaoEncontradoException, SQLException {
+        if(repository.buscarPorId(id) == null) {
+            throw new IdNaoEncontradoException("Erro: ID não encontrado!");
         }
+        return repository.reativar(id);
     }
 
-    public List<Itens_Operacao> listarInfo() {
-        try {
-            List<Itens_Operacao> lista = repository.listarInfo();
+    public List<Itens_Operacao> listarInfo() throws NadaInseridoException, SQLException {
+        List<Itens_Operacao> lista = repository.listarInfo();
 
-            if(lista.isEmpty()) {
-                throw new NadaInseridoException("Erro: Nada inserido no banco");
-            }
-
-            return lista;
-        } catch(NadaInseridoException e) {
-            System.out.println(e.getMessage());
-            return null;
+        if(lista.isEmpty()) {
+            throw new NadaInseridoException("Erro: Nada inserido no banco");
         }
+
+        return lista;
     }
 
-    public Itens_Operacao buscarPorId(Long id) {
-        try {
-            Itens_Operacao io = repository.buscarPorId(id);
+    public Itens_Operacao buscarPorId(Long id) throws IdNaoEncontradoException, SQLException {
+        Itens_Operacao io = repository.buscarPorId(id);
 
-            if(io == null) {
-                throw new IdNaoEncontradoException("Erro: ID não encontrado!");
-            }
-            return io;
-        } catch(IdNaoEncontradoException e) {
-            System.out.println(e.getMessage());
-            return null;
+        if(io == null) {
+            throw new IdNaoEncontradoException("Erro: ID não encontrado!");
         }
+        return io;
     }
 }
