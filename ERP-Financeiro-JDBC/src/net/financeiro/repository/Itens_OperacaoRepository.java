fe9e1496bea76a7e6ec089eecd78dd70696293a6
ryan_bylaardt <vonden.ryan@gmail.com>
2026-07-31 20:33:13 -0300

    fix: implementa try-with-resources em Itens_OperacaoRepository

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/Itens_OperacaoRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/Itens_OperacaoRepository.java
@@ -14,41 +14,39 @@ public class Itens_OperacaoRepository {
     public Itens_Operacao inserir(Itens_Operacao ins) throws SQLException{
         String sql = "INSERT INTO Itens_Operacao (quantidade_produtos, id_produto, id_operacao) VALUES (?, ?, ?)";
 
-        PreparedStatement pr = Conexao.connecting().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pr.setInt(1, ins.getQuantidade_produtos());
+            pr.setLong(2, ins.getId_produto());
+            pr.setLong(3, ins.getId_operacao());
+            pr.executeUpdate();
 
-        pr.setInt(1, ins.getQuantidade_produtos());
-        pr.setLong(2, ins.getId_produto());
-        pr.setLong(3, ins.getId_operacao());
-        pr.executeUpdate();
+            ResultSet rs = pr.getGeneratedKeys();
+            rs.next();
 
-        ResultSet rs = pr.getGeneratedKeys();
-        rs.next();
+            ins.setId_itens_operacao(rs.getLong("GENERATED_KEY"));
 
-        ins.setId_itens_operacao(rs.getLong("GENERATED_KEY"));
-        pr.close();
-        return ins;
+            return ins;
+        }
     }
 
     public Itens_Operacao atualizar(Itens_Operacao atl, Long id) throws SQLException {
         String sql = "UPDATE Itens_Operacao SET quantidade_produtos = ?, id_produto = ?, id_operacao = ? WHERE id_itens_operacao = ?";
 
-        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)) {
+            pr.setInt(1, atl.getQuantidade_produtos());
+            pr.setLong(2, atl.getId_produto());
+            pr.setLong(3, atl.getId_operacao());
+            pr.setLong(4, id);
+            pr.executeUpdate();
 
-        pr.setInt(1, atl.getQuantidade_produtos());
-        pr.setLong(2, atl.getId_produto());
-        pr.setLong(3, atl.getId_operacao());
-        pr.setLong(4, id);
-        pr.executeUpdate();
-
-        pr.close();
-        return atl;
+            return atl;
+        }
     }
 
     public boolean deletar(Long id) throws SQLException {
         String sql = "update Itens_Operacao set ativo = false WHERE id_itens_operacao = ?";
 
         try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)) {
-
             pr.setLong(1, id);
 
             return pr.executeUpdate() != 0;
@@ -69,36 +67,34 @@ public class Itens_OperacaoRepository {
     public List<Itens_Operacao> listarInfo() throws SQLException {
         String sql = "SELECT Itens_Operacao.*, Produto.nome FROM Itens_Operacao join Produto on Itens_Operacao.id_produto = Produto.id_produto where Itens_Operacao.ativo = true";
 
-        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
 
-        List<Itens_Operacao> lista = new ArrayList<>();
-        ResultSet rs = pr.executeQuery();
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)) {
+            List<Itens_Operacao> lista = new ArrayList<>();
+            ResultSet rs = pr.executeQuery();
 
-        while(rs.next()) {
-            lista.add(new Itens_Operacao(
-                    rs.getLong("id_itens_operacao"),
-                    rs.getLong("id_produto"),
-                    rs.getLong("id_operacao"),
-                    rs.getInt("quantidade_produtos"),
-                    rs.getString("Produto.nome")
-            ));
+            while(rs.next()) {
+                lista.add(new Itens_Operacao(
+                        rs.getLong("id_itens_operacao"),
+                        rs.getLong("id_produto"),
+                        rs.getLong("id_operacao"),
+                        rs.getInt("quantidade_produtos"),
+                        rs.getString("Produto.nome")
+                ));
+            }
+
+            return lista;
         }
-
-        pr.close();
-        return lista;
     }
 
     public Itens_Operacao buscarPorId(Long id) throws SQLException {
         String sql = "SELECT * FROM Itens_Operacao WHERE id_itens_operacao = ? and ativo = true";
 
-        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)) {
+            pr.setLong(1, id);
+            ResultSet rs = pr.executeQuery();
 
-        pr.setLong(1, id);
-        ResultSet rs = pr.executeQuery();
-
-        rs.next();
-        Itens_Operacao result = new Itens_Operacao(id, rs.getLong("id_produto"), rs.getLong("id_operacao"), rs.getInt("quantidade_produtos"));
-        pr.close();
-        return result;
+            rs.next();
+            return new Itens_Operacao(id, rs.getLong("id_produto"), rs.getLong("id_operacao"), rs.getInt("quantidade_produtos"));
+        }
     }
 }
