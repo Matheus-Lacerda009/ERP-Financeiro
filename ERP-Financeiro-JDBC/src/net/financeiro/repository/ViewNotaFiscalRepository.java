61e87c8a014e34ae01d570d8e03abdcd1576fcdc
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-08-01 03:40:48 -0300

    fix: adicionando try com recursos em ViewNotaFiscalRepository

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/ViewNotaFiscalRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/ViewNotaFiscalRepository.java
@@ -12,13 +12,13 @@ import java.util.List;
 public class ViewNotaFiscalRepository {
     public List<ViewNotaFiscal> listarInfo() throws SQLException {
         String sql = "SELECT * FROM ViewNotaFiscal";
-        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
-        ResultSet rs = pr.executeQuery();
-        List<ViewNotaFiscal> lista = new ArrayList<>();
-        while(rs.next()){
-            lista.add(new ViewNotaFiscal(rs.getLong("numero_nota_fiscal"), rs.getLong("id_fluxo_caixa"), rs.getLong("id_parceiro"), rs.getString("data_emissao"), rs.getString("nome_empresa_parceira"), rs.getString("documento_parceiro"), rs.getString("email_parceiro"), rs.getString("forma_pagamento"), rs.getString("banco_recebimento"), rs.getInt("parcelas")));
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)) {
+            ResultSet rs = pr.executeQuery();
+            List<ViewNotaFiscal> lista = new ArrayList<>();
+            while (rs.next()) {
+                lista.add(new ViewNotaFiscal(rs.getLong("numero_nota_fiscal"), rs.getLong("id_fluxo_caixa"), rs.getLong("id_parceiro"), rs.getString("data_emissao"), rs.getString("nome_empresa_parceira"), rs.getString("documento_parceiro"), rs.getString("email_parceiro"), rs.getString("forma_pagamento"), rs.getString("banco_recebimento"), rs.getInt("parcelas")));
+            }
+            return lista;
         }
-        pr.close();
-        return lista;
     }
 }
