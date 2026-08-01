cc5316f3166a72d4632628b20d79366a18143c41
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-08-01 03:42:48 -0300

    fix: adicionando try com recursos em UsuarioRepository

--- ERP-Financeiro-JDBC/src/net/financeiro/repository/UsuarioRepository.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/repository/UsuarioRepository.java
@@ -12,36 +12,31 @@ import java.sql.SQLException;
 public class UsuarioRepository {
     public boolean cadastrando(Usuario ins) throws SQLException {
         String sql = "insert into Usuarios(nome, senha, permissao) values (?, ?, ?)";
-        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
-        pr.setString(1, ins.getNome());
-        pr.setString(2, BCrypt.hashpw(ins.getSenha(), BCrypt.gensalt()));
-        pr.setInt(3, ins.getPermissao());
-        if(pr.executeUpdate() != 0){
-            return true;
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)) {
+            pr.setString(1, ins.getNome());
+            pr.setString(2, BCrypt.hashpw(ins.getSenha(), BCrypt.gensalt()));
+            pr.setInt(3, ins.getPermissao());
+            return pr.executeUpdate() != 0;
         }
-        pr.close();
-        return false;
     }
 
     public boolean isAdm(Usuario adm) throws SQLException {
         String sqlVal = "select id, senha from Usuarios where nome = ?";
-        PreparedStatement prVal = Conexao.connecting().prepareStatement(sqlVal);
-        prVal.setString(1, adm.getNome());
-        ResultSet rs = prVal.executeQuery();
-        rs.next();
-        boolean resultado = rs.getLong("id") == 1L && BCrypt.checkpw(adm.getSenha(), rs.getString("senha"));
-        prVal.close();
-        return resultado;
+        try(PreparedStatement prVal = Conexao.connecting().prepareStatement(sqlVal)) {
+            prVal.setString(1, adm.getNome());
+            ResultSet rs = prVal.executeQuery();
+            rs.next();
+            return rs.getLong("id") == 1L && BCrypt.checkpw(adm.getSenha(), rs.getString("senha"));
+        }
     }
 
     public boolean validacao(Usuario user) throws SQLException {
         String sql = "select senha from Usuarios where nome = ?";
-        PreparedStatement pr = Conexao.connecting().prepareStatement(sql);
-        pr.setString(1, user.getNome());
-        ResultSet rs = pr.executeQuery();
-        rs.next();
-        boolean resultado = BCrypt.checkpw(user.getSenha(), rs.getString("senha"));
-        pr.close();
-        return resultado;
+        try(PreparedStatement pr = Conexao.connecting().prepareStatement(sql)) {
+            pr.setString(1, user.getNome());
+            ResultSet rs = pr.executeQuery();
+            rs.next();
+            return BCrypt.checkpw(user.getSenha(), rs.getString("senha"));
+        }
     }
 }
