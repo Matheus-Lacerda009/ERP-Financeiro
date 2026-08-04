0d72772db90dc1cb0f394723871c1cc3d6ec9004
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-08-04 01:38:41 -0300

    fix: adiciona throws em usuario controller

--- ERP-Financeiro-JDBC/src/net/financeiro/controller/UsuarioController.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package net.financeiro.controller;
 
+import net.financeiro.exceptions.NadaInseridoException;
+import net.financeiro.exceptions.ValorInvalidoException;
 import net.financeiro.model.Usuario;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import net.financeiro.service.UsuarioService;
+
+import java.sql.SQLException;
 
 @RestController
 @CrossOrigin("*")
@@ -12,12 +16,12 @@ public class UsuarioController {
     private final UsuarioService service = new UsuarioService();
 
     @PostMapping
-    public ResponseEntity<Boolean> cadastrando(@RequestBody Usuario adm, @RequestBody Usuario ins){
+    public ResponseEntity<Boolean> cadastrando(@RequestBody Usuario adm, @RequestBody Usuario ins) throws NadaInseridoException, SQLException, ValorInvalidoException {
         return ResponseEntity.ok(service.cadastrando(adm, ins));
     }
 
     @GetMapping
-    public ResponseEntity<Boolean> validacao(@RequestParam String nome, @RequestParam String senha){
+    public ResponseEntity<Boolean> validacao(@RequestParam String nome, @RequestParam String senha) throws NadaInseridoException, SQLException {
         Usuario log = new Usuario(nome, senha);
         return ResponseEntity.ok(service.validacao(log));
     }
