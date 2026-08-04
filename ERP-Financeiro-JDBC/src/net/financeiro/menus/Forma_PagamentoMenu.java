2736f3835d71327faab0ec20643446cf050e499a
ryan_bylaardt <vonden.ryan@gmail.com>
2026-08-04 17:40:04 -0300

    feat: implementa verificação de permissão em FuncionarioMenu

--- ERP-Financeiro-JDBC/src/net/financeiro/menus/Forma_PagamentoMenu.java
+++ ERP-Financeiro-JDBC/src/net/financeiro/menus/Forma_PagamentoMenu.java
@@ -5,6 +5,7 @@ import com.williamcallahan.tui4j.compat.bubbles.viewport.Viewport;
 import com.williamcallahan.tui4j.compat.bubbletea.*;
 import com.williamcallahan.tui4j.compat.lipgloss.Style;
 import com.williamcallahan.tui4j.compat.lipgloss.color.AdaptiveColor;
+import net.financeiro.connection.VerificarPermissao;
 import net.financeiro.menus.events.JdbcQueryResult;
 import net.financeiro.model.Forma_Pagamento;
 import net.financeiro.service.Forma_PagamentoService;
@@ -114,21 +115,24 @@ public class Forma_PagamentoMenu implements TableMenu {
                         break;
                     case "n":
                         //inicia a criação de uma nova entidade
-                        //todo fazer verificação de permissão antes de continuar ação
-                        estado = Estado.CRIANDO;
-                        iniciarFormulario();
+                        if(VerificarPermissao.validar("W")) {
+                            estado = Estado.CRIANDO;
+                            iniciarFormulario();
+                        }
                         break;
                     case "f":
                         //inicia a edição da entidade selecionada
-                        //todo fazer verificação de permissão antes de continuar ação
-                        estado = Estado.EDITANDO;
-                        iniciarFormulario();
+                        if(VerificarPermissao.validar("W")) {
+                            estado = Estado.EDITANDO;
+                            iniciarFormulario();
+                        }
                         break;
                     case "x":
                         //deleta a entidade selecionada;
-                        //todo fazer verificação de permissão antes de continuar ação
-                        estado = Estado.DELETANDO;
-                        atualizarViewport();
+                        if(VerificarPermissao.validar("D")) {
+                            estado = Estado.DELETANDO;
+                            atualizarViewport();
+                        }
                         break;
                     case "ctrl+r":
                         return consultarBanco();
